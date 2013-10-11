606406b17b5bd73368c6f765fde0e26049da827c
Brandon Thomas <bt@brand.io>
2013-10-11 02:21:43 -0400

    A little headway on broadcast thread, etc.

---
+++ broadcast.py
@@ -0,0 +1,73 @@
+import struct
+from uuid import getnode as get_mac
+
+class BroadcastPacket(object):
+	"""
+	Represents an EtherDream broadcast packet.
+	For example, my EtherDream outputs:
+		- MAC: 00:04:a3:87:28:cd
+		- HW 2, SW 2
+		- Capabilities: max 1799 points, 100000 kpps
+		- Light engine: state 0, flags 0x0
+		- Playback: state 0, flags 0x0
+		- Buffer: 0 points
+		- Playback: 0 kpps, 0 points played
+		- Source: 0, flags 0x0
+	"""
+
+	def __init__(self):
+		self.mac = get_mac()
+		self.hw_rev = 2
+		self.sw_rev = 2
+		self.buffer_cap = 1799
+		self.max_pt_rate = 100000
+		self.proto = 0
+		self.le_state = 0
+		self.play_state = 0
+		self.source = 0
+		self.le_flags = 0
+		self.play_flags = 0
+		self.source_flags = 0
+		self.fullness = 0
+		self.point_rate = 0
+		self.point_count = 0
+
+	def getStruct(self):
+		byts = []
+		mac = self.mac
+		while mac > 0:
+			d = mac & 0xFF
+			byts.append('%02x' %d)
+			mac >>= 8
+
+		byteStr = int(''.join(byts), 16)
+
+		struct_mac = struct.pack('<' + 'Q', byteStr)[:6] # Two wasted bits
+
+		hw_rev = int('%02x' % self.hw_rev, 16)
+		sw_rev = int('%02x' % self.sw_rev, 16)
+		buffer_cap = int('%02x' % self.buffer_cap, 16)
+		max_pt_rate= int('%04x' % self.max_pt_rate, 16)
+
+		struct_info = struct.pack('<HHHI', hw_rev, sw_rev, \
+							buffer_cap, max_pt_rate)[:10]
+
+		proto = self.proto
+		le_state = self.le_state
+		play_state = self.play_state
+		source = self.source
+
+		le_flags = self.le_flags
+		play_flags  = self.play_flags
+		source_flags = self.source_flags
+
+		fullness = self.fullness
+		point_rate = self.point_rate
+		point_count = self.point_count
+
+		struct_status =  struct.pack('<BBBBHHHHII', \
+				proto, le_state, play_state, source, le_flags, play_flags, \
+				source_flags, fullness, point_rate, point_count)[:20]
+
+		return struct_mac + struct_info + struct_status
+
--- main.py
+++ main.py
@@ -1,84 +1,32 @@
 #!/usr/bin/env python
 
 import struct
+import time
 import base64
 from socket import *
 from uuid import getnode as get_mac
+from multiprocessing import Process, Queue
 
-"""
-My EtherDeam's Output
-=====================
-- MAC: 00:04:a3:87:28:cd
-- HW 2, SW 2
-- Capabilities: max 1799 points, 100000 kpps
-- Light engine: state 0, flags 0x0
-- Playback: state 0, flags 0x0
-- Buffer: 0 points
-- Playback: 0 kpps, 0 points played
-- Source: 0, flags 0x0
-"""
+from broadcast import BroadcastPacket
 
-def packed_mac(mac=None):
-	if not mac:
-		mac = get_mac()
-
-	byts = []
-	while mac > 0:
-		d = mac & 0xFF
-		byts.append('%02x' %d)
-		mac >>= 8
-
-	byteStr = int(''.join(byts), 16)
-
-	return struct.pack('<' + 'Q', byteStr)[0:6] # Two wasted bits
-
-def packed_info():
-	hw_rev = int('%02x' % 2, 16)
-	sw_rev = int('%02x' % 2, 16)
-	buffer_cap = int('%02x' % 1799, 16)
-	max_pt_rate= int('%04x' % 100000, 16)
-
-	return struct.pack('<HHHI', hw_rev, sw_rev, buffer_cap, max_pt_rate)[0:10]
-
-def packed_status():
-	proto = 0
-	le_state = 0
-	play_state = 0
-	source = 0
-
-	le_flags = 0
-	play_flags  = 0
-	source_flags = 0
-
-	fullness = 0
-	point_rate = 0
-	point_count = 0
-
-	return struct.pack('<BBBBHHHHII', \
-			proto, le_state, play_state, source, le_flags, play_flags, \
-			source_flags, fullness, point_rate, point_count)
-
-def main():
+def broadcast_thread():
 	s = socket(AF_INET, SOCK_DGRAM)
 	s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 	s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
-	mac = packed_mac()
-	info = packed_info()
-	status = packed_status()
-
-	broadcastPacket = mac + info + status
+	bp = BroadcastPacket()
+	broadcastPacket = bp.getStruct()
 
 	while 1:
 		#s.sendto(broadcastPacket, ('255.255.255.255', 7654))
 		#s.sendto(broadcastPacket, ('', 7654))
 		s.sendto(broadcastPacket, ('localhost', 7654))
 
-def listen():
+
+# TODO TODO TODO
+def etherdream_thread():
 	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-	server.bind(('', 80))
-
+	server.bind(('', 7765))
 	server.listen(3)
 
 	while 1:
@@ -88,6 +36,16 @@ def listen():
 		#in this case, we'll pretend this is a threaded server
 		ct = client_thread(clientsocket)
 		ct.run()
+
+def main():
+	p1 = Process(target=broadcast_thread, args=())
+	p1.start()
+
+	p2 = Process(target=etherdream_thread, args=())
+	p2.start()
+
+	while True:
+		time.sleep(100000)
 
 if __name__ == '__main__':
 	main()
