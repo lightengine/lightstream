abf93a0ee85d187688afe6a3a00c68f61ce1abd6
Brandon Thomas <bt@brand.io>
2013-10-13 15:53:59 -0400

    Dac can send "hello" response

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import struct
+import threading
 import time
 import base64
 from socket import *
@@ -8,8 +9,9 @@ from uuid import getnode as get_mac
 from multiprocessing import Process, Queue
 
 from broadcast import BroadcastPacket
+from etherdream import EtherdreamThread, SocketBroken
 
-def broadcast_thread():
+def broadcast_process():
 	s = socket(AF_INET, SOCK_DGRAM)
 	s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 	s.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
@@ -23,25 +25,33 @@ def broadcast_thread():
 		s.sendto(broadcastPacket, ('localhost', 7654))
 
 
-# TODO TODO TODO
-def etherdream_thread():
-	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	server.bind(('', 7765))
-	server.listen(3)
+def etherdream_process():
+	s = socket(AF_INET, SOCK_STREAM)
+	s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+	s.bind(('localhost', 7765))
+	s.listen(3)
 
 	while 1:
 		#accept connections from outside
-		(clientsocket, address) = serversocket.accept()
+		(clientsocket, address) = s.accept()
 		#now do something with the clientsocket
 		#in this case, we'll pretend this is a threaded server
-		ct = client_thread(clientsocket)
-		ct.run()
+		#msg = receive(s)
+		print "accepted: %s" % str(address)
+		try:
+			cs = EtherdreamThread(clientsocket, address)
+			cs.communicate()
+		except SocketBroken as e:
+			print e
+			pass
+		#ct = client_thread(clientsocket)
+		#ct.run()
 
 def main():
-	p1 = Process(target=broadcast_thread, args=())
+	p1 = Process(target=broadcast_process, args=())
 	p1.start()
 
-	p2 = Process(target=etherdream_thread, args=())
+	p2 = Process(target=etherdream_process, args=())
 	p2.start()
 
 	while True:
