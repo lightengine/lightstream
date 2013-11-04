f792a04231f60f9cda1b162760ddcf110b30a87b
Brandon Thomas <bt@brand.io>
2013-11-04 09:45:28 -0500

    Remove ref code

--- circular.py
+++ circular.py
@@ -1,35 +1,3 @@
-"""
-void cbInit(CircularBuffer *cb, int size) {
-    cb->size  = size + 1; /* include empty elem */
-    cb->start = 0;
-    cb->end   = 0;
-    cb->elems = (ElemType *)calloc(cb->size, sizeof(ElemType));
-}
-
-void cbFree(CircularBuffer *cb) {
-    free(cb->elems); /* OK if null */ }
-
-int cbIsFull(CircularBuffer *cb) {
-    return (cb->end + 1) % cb->size == cb->start; }
-
-int cbIsEmpty(CircularBuffer *cb) {
-    return cb->end == cb->start; }
-
-/* Write an element, overwriting oldest element if buffer is full. App can
-   choose to avoid the overwrite by checking cbIsFull(). */
-void cbWrite(CircularBuffer *cb, ElemType *elem) {
-    cb->elems[cb->end] = *elem;
-    cb->end = (cb->end + 1) % cb->size;
-    if (cb->end == cb->start)
-        cb->start = (cb->start + 1) % cb->size; /* full, overwrite */
-}
-
-/* Read oldest element. App must ensure !cbIsEmpty() first. */
-void cbRead(CircularBuffer *cb, ElemType *elem) {
-    *elem = cb->elems[cb->start];
-    cb->start = (cb->start + 1) % cb->size;
-}
-"""
 
 class CircularByteBuffer(object):
 	SZ = 1024
@@ -69,5 +37,3 @@ class CircularByteBuffer(object):
 		self._start = (self._start + self.SZ) % self._length
 		return view
 
-	def pt(self):
-		print str(self._buf)
